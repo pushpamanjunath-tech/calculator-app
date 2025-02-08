e63096c699ef199d834301a263ca16d0d509def3
Pushpa <pushpamanjunath656@gmail.com>
2025-02-08 19:35:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    let display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    let display = document.getElementById('display');
+    if (display.value !== '') {
+        display.value += operator;
+    }
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
